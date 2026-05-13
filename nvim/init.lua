1c65501f3fd9c8daf7d597cbe9d47acf1ab70912
quang11224 <189814570+quang11224@users.noreply.github.com>
2026-05-13 13:04:16 +0700

    migrate to lazy vim plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,19 @@
-require("quang.core")
-require("quang.lazy")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+vim.notify = function(msg, level, opts)
+  if type(msg) ~= "string" then
+    return
+  end
+
+  local hl = "None"
+  if level == vim.log.levels.WARN then
+    hl = "WarningMsg"
+  elseif level == vim.log.levels.ERROR then
+    hl = "ErrorMsg"
+  elseif level == vim.log.levels.INFO then
+    hl = "None"
+  end
+
+  vim.api.nvim_echo({ { msg, hl } }, true, {})
+end
